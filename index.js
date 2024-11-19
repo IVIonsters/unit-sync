5d0262b1bc7e12416b8a0c62c448fc745a42fa63
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-11-18 21:41:27 -0600

    created main variables , grabbed user input on convert button click logged

---
+++ index.js
@@ -0,0 +1,25 @@
+// Main Variables
+let input = document.getElementById('user-input');
+let convertBtn = document.getElementById('convert-btn');
+
+// Grab user input from input box when convert button is clicked
+function userInput() {
+  let user = input.value;
+  console.log(user);
+}
+convertBtn.addEventListener('click', userInput);
+
+
+//functions to converter user input from metric to imperial
+
+
+// Meter to Feet
+
+
+// liters to gallons
+
+
+// kilogram to pounds
+
+
+// render to various p tags in the html file
