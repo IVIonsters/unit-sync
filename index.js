a9d2705ce3f626f09af149b10e286b14b086e782
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-11-19 18:41:20 -0600

    restructured function for meterToFeet and UserInput to display conversion to feet in result's box for testing

--- index.js
+++ index.js
@@ -5,25 +5,45 @@ let convertBtn = document.getElementById('convert-btn');
 // Grab user input from input box when convert button is clicked
 function userInput() {
   let user = Number(input.value);
-  //add if statement to check if user input is a number
+  // Check if user input is a number
   if (!isNaN(user)) {
     console.log("User input is " + user + " and is a " + typeof user);
+    return user;
   } else {
     alert('Please enter a number');
+    return null;
   }
 }
-convertBtn.addEventListener('click', userInput);
 
-//functions to converter user input from metric to imperial
+// Function to convert meters to feet
+function meterToFeet(user) {
+  if (user !== null) {
+    let feet = user * 3.28084;
+    console.log(user + " meters is equal to " + feet + " feet");
+    displayResult(user + " meters is equal to " + feet + " feet");
+  }
+}
 
+// Function to display result in the DOM
+function displayResult(message) {
+  const resultContainer = document.getElementById('result');
+  resultContainer.textContent = message;
+}
 
-// Meter to Feet
-
+// Add event listener to the convert button
+convertBtn.addEventListener('click', function () {
+  let user = userInput();
+  meterToFeet(user);
+});
 
 // liters to gallons
+function litersToGallon() {
 
+}
 
 // kilogram to pounds
+function kilogramToPounds() {
 
+}
 
 // render to various p tags in the html file
